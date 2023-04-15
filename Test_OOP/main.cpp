a3592f9a9f997fe440cca03e03eeab0495282f1b
danuleca <daniel.g.ikonomov.2020@elsys-bg.org>
2023-04-15 22:42:46 +0200

    GolqmoDomashno - DONE

--- Test_OOP/main.cpp
+++ Test_OOP/main.cpp
@@ -6,60 +6,84 @@ using namespace std;
 class TaskList
 {
 private:
-    string *tasks;
+    const string *tasks;
     int numberOfTasks;
-    char *name;
+    const char *name;
+
+    static const char * dupCStr(const char *name){
+        int len = strlen(name);
+        char *result = new char[len];
+        for(int i = 0; i<len; i++){
+            result[i] = name[i];
+        }
+        return result;
+    }
+    static const string * dupTsk(const string *tasks, int numberOfTasks){
+        string *result = new string[numberOfTasks];
+        for(int i = 0; i<numberOfTasks; i++){
+            result[i] = tasks[i];
+        }
+        return result;
+    }
 
 protected:
     static int numberOfLists;
 
 public:
-    TaskList()
-    :tasks(0), name(0)
-    {}
-
-    TaskList(char *name, string *tasks, int numberOfTasks)
-    : name(name), tasks(tasks), numberOfTasks(numberOfTasks)
-    {
-
-        if(numberOfTasks >= 0) throw invalid_argument("Number of Tasks <= 0!");
-        if(name == nullptr) throw invalid_argument("Nullptr!");
-        if(tasks == nullptr) throw invalid_argument("Nullptr!");
+    TaskList(): tasks(new string[1]), name("") {
         numberOfLists++;
     }
-    
-    TaskList(const TaskList &other)
-    :
-        tasks(other.tasks), numberOfTasks(other.numberOfTasks), name(name)
+
+    TaskList(const char *name, const string *tasks, int numberOfTasks)
+        : name(name), tasks(tasks), numberOfTasks(numberOfTasks)
     {
-         numberOfLists++;
+        if(numberOfTasks <= 0) throw invalid_argument("Number of Tasks <= 0! ");
+        if(tasks == nullptr || name == nullptr) throw invalid_argument("Nullptr!");
+        numberOfLists++;
+    }
+
+    TaskList(const TaskList &other)
+        //: name(strdup(other.name)), tasks{new string[other.numberOfTasks]}, numberOfTasks(other.numberOfTasks)
+        : name(dupCStr(other.name)), tasks(dupTsk(other.tasks, other.numberOfTasks)), numberOfTasks(other.numberOfTasks)
+    {
+        numberOfLists++;
     }
 
     ~TaskList()
     {
-        delete[] this->name;
+        if (tasks != nullptr) delete[] tasks;
+        if (name != nullptr) delete[] name;
         numberOfLists--;
     }
 
     TaskList &operator=(const TaskList &other)
     {
-        if(this != &other)
+        if(&other != this)
         {
             delete[] name;
-            this->tasks = other.tasks;
+            this->name = dupCStr(other.name);
+            this->tasks = dupTsk(other.tasks, other.numberOfTasks);
             this->numberOfTasks = other.numberOfTasks;
-            this->name = new char[numberOfTasks];
-            this->name = strdup(other.name);
+            // TaskList copy(other);
+            // swap(tasks, copy.tasks);
+            // swap(name, copy.name);
+            // swap(numberOfTasks, copy.numberOfTasks);
         }
         return *this;
     }
 
-    static int getActiveLists()
+    void printListInformation() const
     {
-        return numberOfLists;
+        cout << "Number of tasks: " << numberOfTasks << "\n"<< endl;
+        cout << "Tasks: " << *tasks << "\n"<< endl;
+        cout << "Name: " << name << "\n" << endl;
     }
-
+    static int getActiveLists() {return numberOfLists;}
+    string getTasks() const {return *tasks;}
+    int getNumberOfTasks() const {return numberOfTasks;}
+    const char *getName() const {return name;}
 };
+
 int TaskList::numberOfLists = 0;
 
 class ShoppingList : public TaskList
@@ -69,27 +93,27 @@ private:
     int numberOfBoughtProducts;
    
 public:
-    ShoppingList(char *name, string *productsToBuy, int numberOfProductsToBuy, const string *boughtProducts, int numberOfBoughtProducts)
-        : TaskList(name, productsToBuy, numberOfProductsToBuy), boughtProducts(new string[numberOfBoughtProducts]), numberOfBoughtProducts(numberOfBoughtProducts)
+    ShoppingList(const char *name, const string *productsToBuy, int numberOfProductsToBuy, string *boughtProducts, int numberOfBoughtProducts)
+    : TaskList(name, productsToBuy, numberOfBoughtProducts), boughtProducts(boughtProducts), numberOfBoughtProducts(numberOfBoughtProducts)
     {
-        for(int i=0; i<numberOfBoughtProducts; i++)
-        {
-            this->boughtProducts[i] = boughtProducts[i];
-        }
+        if(boughtProducts == nullptr) throw invalid_argument("Nullptr!");
+        if(numberOfBoughtProducts <= 0) throw invalid_argument("Number of bought products <= 0!");
         numberOfLists++;
     }
 
     ShoppingList(const ShoppingList &other)
-    :
-        TaskList(other), boughtProducts(other.boughtProducts), numberOfBoughtProducts(other.numberOfBoughtProducts)
-    {}
+    :   TaskList(other), boughtProducts(other.boughtProducts), numberOfBoughtProducts(other.numberOfBoughtProducts)
+    {
+       numberOfLists++;
+    }
+
     ShoppingList &operator=(const ShoppingList &other)
     {
         if(this != &other)
         {
             ShoppingList::operator=(other);
-            this->boughtProducts = boughtProducts;
-            this->numberOfBoughtProducts = numberOfBoughtProducts;
+            this->boughtProducts = other.boughtProducts;
+            this->numberOfBoughtProducts = other.numberOfBoughtProducts;
         }
         return *this;
     }
@@ -99,17 +123,35 @@ public:
         delete[] boughtProducts;
         numberOfLists--;
     }
-   
+
     void printListInformation() const
     {
-        
+        cout << "Number of products to buy: " << TaskList::getNumberOfTasks() << "\n" << endl;
+        cout << "Products to buy: " << TaskList::getTasks() << "\n" << endl;
+        cout << "Name: " << TaskList::getName() << "\n" << endl;
+        cout << "Bought prodcuts: " << *boughtProducts << "\n" << endl;
+        cout << "Number of bought products: " << numberOfBoughtProducts << "\n" << endl;
     }
 };
 
-
-
-
 int main()
 {
+    // create a TaskList object and print its information
+    string tasks[3] = {"Task 1", "Task 2", "Task 3"};
+    TaskList taskList("My Task List", tasks, 3);
+    taskList.printListInformation();
 
+    // create a ShoppingList object and print its information
+    string productsToBuy[2] = {"Product 1", "Product 2"};
+    string boughtProducts[2] = {"Product 3", "Product 4"};
+    ShoppingList shoppingList("My Shopping List", productsToBuy, 2, boughtProducts, 2);
+    shoppingList.printListInformation();
+
+    // test the copy constructor
+    ShoppingList shoppingListCopy(shoppingList);
+    shoppingListCopy.printListInformation();
+
+    // test the assignment operator
+    ShoppingList shoppingListCopy2 = shoppingList;
+    shoppingListCopy2.printListInformation();
 }
